af5e807d31d01330e28165e40915bdd1f039cb8a
Isuru <isurumaduranga.official@gmail.com>
2019-04-29 04:38:30 +0530

    correct an error

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 
-app.use('/', express.static('dist'));
+app.use('/', express.static(__dirname + '/dist/LandChain-Frontend'));
 
 app.listen(8080, () => {
     console.log(`server started on port 8080`);
