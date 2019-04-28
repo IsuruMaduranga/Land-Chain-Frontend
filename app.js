996ac65ca1006e3406bcb6461503116c8642417b
Isuru <isurumaduranga.official@gmail.com>
2019-04-29 04:31:30 +0530

    correct an error

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ const app = express();
 app.use('/', express.static('dist'));
 
 app.listen(8080, () => {
-    console.log(`server started on port ${port}`);
+    console.log(`server started on port 8080`);
 });
